b41be93513f041ad9a3b1e5ef57aa3a4b1f5371a
John Caprez <jay@jaykay-design.com>
2018-05-05 18:56:03 +0200

    get repository url as parameter

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -x -e
 docker build --no-cache -t "$1"/php:7.2.1-apache -t "$1"/php:latest 7.2.1-apache
 docker tag "$1"/php:latest "$1"/php:latest
 
-docker login -u "$2" -p "$3" jaykay.azurecr.io
+docker login -u "$2" -p "$3" "$1"
 
 docker push "$1"/php:latest
 docker push "$1"/php:7.2.1-apache
